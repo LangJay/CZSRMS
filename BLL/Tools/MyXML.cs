3bfd933af3255d435d3e1aa1cc44cf44ca6963d4
Jimmy <zjl_hnyz@163.com>
2017-08-16 15:01:48 +0800

    分类设置完成

--- BLL/Tools/MyXML.cs
+++ BLL/Tools/MyXML.cs
@@ -151,5 +151,64 @@ namespace BLL.Tools
             }
             return true;
         }
+        public static bool EditElement(string oldkind,Category category)
+        {
+            try
+            {
+                XElement xm = XElement.Load(path);
+                IEnumerable<XElement> elements = from ele in xm.Elements("kind") where ele.Attribute("Type").Value == oldkind select ele;
+                {
+                    if (elements.Count() > 0)
+                    {
+                        XElement x = elements.First();
+                        x.Attribute("Type").Value = category.kind;
+                        x.Attribute("Select").Value = category.select;
+                        x.RemoveNodes();
+                        foreach (string n in category.nodes)
+                        {
+                            XElement node = new XElement(new XElement("node", new XAttribute("name", n)));
+                            x.Add(node);
+                        }
+                    }
+                }
+                xm.Save(path);
+            }
+            catch (Exception e)
+            {
+                Log.AddRecord(e);
+                return false;
+            }
+            return true;
+        }
+        public static Category GetElement(string kind)
+        {
+            Category category = new Category();
+            try
+            {
+                XElement xm = XElement.Load(path);
+                IEnumerable<XElement> elements = from ele in xm.Elements("kind") where ele.Attribute("Type").Value == kind select ele;
+                {
+                    foreach (XElement x in elements)
+                    {
+                        category.kind = x.Attribute("Type").Value;
+                        category.select = x.Attribute("Select").Value;
+                        //获取所有子节点
+                        IEnumerable<XElement> nodes = x.Elements("node");
+                        foreach (XElement n in nodes)
+                        {
+                            string value = n.Attribute("name").Value;
+                            category.nodes.Add(n.Attribute("name").Value);
+                        }
+                        return category;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Log.AddRecord(e);
+                return null;
+            }
+            return null;
+        }
     }
 }
--- SurveyingResultManageSystem/Controllers/CategoryController.cs
+++ SurveyingResultManageSystem/Controllers/CategoryController.cs
@@ -2,6 +2,7 @@
 using BLL.Tools;
 using Model;
 using Newtonsoft.Json;
+using SurveyingResultManageSystem.App_Start;
 using SurveyingResultManageSystem.Models;
 using System;
 using System.Collections.Generic;
@@ -21,6 +22,7 @@ namespace SurveyingResultManageSystem.Controllers
         }
 
         // GET: Category
+        [Authentication]
         public ActionResult CategoryIndex()
         {
             string operation = LogOperations.UploadFile() + LogOperations.DownloadFile() + LogOperations.DeleteFile();
@@ -33,6 +35,7 @@ namespace SurveyingResultManageSystem.Controllers
             ViewBag.All = MyXML.GetAllCategory();
             return View();
         }
+        [Authentication]
         [HttpPost]
         public ActionResult AddCategory()
         {
@@ -59,6 +62,79 @@ namespace SurveyingResultManageSystem.Controllers
                 return Content("添加失败！");
             }
             return Content("添加成功！");
+        }
+        [Authentication]
+        [HttpGet]
+        public ActionResult DeleteCategory(string kind)
+        {
+            bool success = MyXML.DeleteElement(kind);
+            if (!success) return Content("删除失败！");
+            return Content("删除成功！");
+        }
+        [Authentication]
+        [HttpGet]
+        public string GetCategory(string kind)
+        {
+            Category category = MyXML.GetElement(kind);
+            string json = null;
+            if (category != null)
+            {
+                try
+                {
+                    json = JsonConvert.SerializeObject(category);
+                }
+                catch(Exception e)
+                {
+                    Log.AddRecord(e);
+                    return json;
+                }
+            }
+            return json;
+        }
+        [Authentication]
+        [HttpPost]
+        public ActionResult EditCategory()
+        {
+            var sr = new StreamReader(Request.InputStream);
+            var stream = sr.ReadToEnd();
+            sr.Close();
+            try
+            {
+                Category category = JsonConvert.DeserializeObject<Category>(stream) as Category;
+                Category newcategory = category;
+                //取出新旧名
+                string oldname = category.kind.Split('^')[0];
+                Category oldcategory = MyXML.GetElement(oldname);
+                string newname = category.kind.Split('^')[1];
+                newcategory.kind = newname;
+                newcategory.select = oldcategory.select;
+                bool success = MyXML.EditElement(oldname, newcategory);
+                if (!success) return Content("编辑失败！");
+            }
+            catch (Exception e)
+            {
+                Log.AddRecord(e);
+                return Content("编辑失败！");
+            }
+            return Content("编辑成功！");
+        }
+        [Authentication]
+        [HttpGet]
+        public ActionResult EditCategorySelect(string select,string kind)
+        {
+            try
+            {
+                Category oldcategory = MyXML.GetElement(kind);
+                oldcategory.select = select;
+                bool success = MyXML.EditElement(kind, oldcategory);
+                if (!success) return Content("编辑失败！");
+            }
+            catch (Exception e)
+            {
+                Log.AddRecord(e);
+                return Content("编辑失败！");
+            }
+            return Content("编辑成功！");
         }
     }
 }
